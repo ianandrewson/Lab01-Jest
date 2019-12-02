28f037cd424ff68ad1f02cc66bda224edc5b37c3
Ian Andrewson <ianandrewson@gmail.com>
2019-12-02 13:54:50 -0800

    tests fixed; ready to begin TDD

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -14,6 +14,21 @@ describe('validator module', () => {
       expect(isNumber(() => {})).toBeFalsy();
       expect(isNumber(true)).toBeFalsy();
     });
+    it('properly tells if a value is a string', () => {
+
+    });
+    it('propely tells if a value is a boolean', () => {
+
+    });
+    it('properly tells if a value is an array', () => {
+
+    });
+    it('properly tells if a value is an object', () => {
+
+    });
+    it('properly tells if a value is a function', () => {
+
+    });
   });
 
   describe('casters', () => {
@@ -23,15 +38,38 @@ describe('validator module', () => {
       expect(castToNumber(true)).toEqual(1);
       expect(castToNumber(false)).toEqual(0);
     });
-
     it('throws if value is not castable to number', () => {
       expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
       expect(() => castToNumber({})).toThrowErrorMatchingSnapshot();
+    });
+
+    it('can cast values to a string', () => {
+
+    });
+    it('throws if value is not castable to a string', () => {
+
+    });
+
+    it('can cast values to a boolean', () => {
+
+    });
+    it('throws if value is not castable to a boolean', () => {
+
+    });
+
+    it('can cast values to an array', () => {
+
+    });
+    it('throws if value is not castable to an array', () => {
+
     });
   });
 
   it('can get the right caster', () => {
     expect(getCaster(Number)).toEqual(castToNumber);
     expect(getCaster(Promise)).toBeNull();
+    // expect(getCaster(String)).toEqual(castToString);
+    // expect(getCaster(Boolean)).toEqual(castToBoolean);
+    // expect(getCaster(Array)).toEqual(castToArray);
   });
 });
